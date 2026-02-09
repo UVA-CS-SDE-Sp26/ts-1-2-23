d2fdf8c5afd7b31be75832de8cf1d58c32cca9da
Nassib Rayes <nassib146@gmail.com>
2026-02-09 15:26:54 -0500

    Filehandler respective classes added (txt class mostly complete and test ideas written down)

---
+++ src/test/java/FilehandlerTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.logging.FileHandler;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilehandlerTest {
+    //Test 1: Verify whether listFiles returns all file names when files exist
+
+
+
+    //Test 2: Verify that readFiles returns file contents.
+
+
+    //Test 3: Verify that readFiles throws an error when the file doesn't exist
+
+
+    //Test 4: Verfy that listFiles throws an error if the data directory doesn't exist
+}
+
