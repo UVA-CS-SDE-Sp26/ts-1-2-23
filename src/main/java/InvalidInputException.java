c6a1d2c73c045b6dbe38f4eb6c3e895ccd7c7637
Michael Chung <mung.chung@outlook.com>
2026-02-12 15:50:15 -0500

    added userinterface documentation

--- src/main/java/InvalidInputException.java
+++ src/main/java/InvalidInputException.java
@@ -1,6 +1,11 @@
+/**
+ * Exception handler for invalid inputs
+ */
 public class InvalidInputException extends Exception{
 
-
+    /**
+     * Default exception handler method
+     */
     public InvalidInputException() {
         super("Invalid input.");
     }
--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -1,32 +1,51 @@
-import java.util.stream.Stream;
 
+/**
+ * User interface class for terminal use of TopSecret program
+ */
 public class Userinterface {
 
-    private Programcontrol pc =  new Programcontrol();
+    private Programcontrol pc =  new Programcontrol(); //program control instantiation
 
+    /**
+     * default constructor
+     */
     public Userinterface() {
     }
 
+    /**
+     * Starts the program
+     * @param args
+     * @return String for output, from program control
+     * @throws InvalidInputException
+     */
     public String startProgram(String[] args) throws InvalidInputException {
-        if ((args.length > 1)) {
+        if ((args.length > 1)) { //if too many arguments throw error
             throw new InvalidInputException("Too many arguments. Accepted format: java TopSecret [argument]");
         } else {
-            if (args.length == 0) {
+            if (args.length == 0) { //if no arguments just list files
                 return this.pc.listFiles();
             }
-            String argument = args[0];
-            if (argument.chars().allMatch(Character::isDigit)) {
-                return this.pc.handleRequest(argument);
+            String argument = args[0]; //there should only be one argument
+            if (argument.chars().allMatch(Character::isDigit)) { //ensure argument is valid (no characters)
+                return this.pc.handleRequest(argument); //pass on request to program controller
             } else {
                 throw new InvalidInputException("Invalid argument.");
             }
         }
     }
 
+    /**
+     * Program control getter
+     * @return pc
+     */
     public Programcontrol getPc() {
         return pc;
     }
 
+    /**
+     * Program control setter
+     * @param pc
+     */
     public void setPc(Programcontrol pc) {
         this.pc = pc;
     }
